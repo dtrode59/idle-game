033544221d8296a96af28732513b7a45c4938ae6
Travis <dtrode59@harrisonburg.k12.va.us>
2019-09-18 08:54:08 -0400

    Update game.js

--- game.js
+++ game.js
@@ -4,11 +4,14 @@ postToPage(score); // Print to the page
 
 createButton('Button', btnPress); // Make a button
 changeTitle('Clicker Game');
-
+createButton('button', btnPress);
+function btnpress(){
+ score--;
+ postToPage(score); 
+}
 setInterval(btnPress, 1000);
 
 function btnPress() {
   score++;
   postToPage(score);
 }
-
