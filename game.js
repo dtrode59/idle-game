d034d28a7b341d13eecc348ed8bc045be7a6da23
Travis <dtrode59@harrisonburg.k12.va.us>
2019-09-26 09:54:17 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 var score = 1; // Create a variable to store the score in
 
-var title = new Title('Welcome to the Game'); // Create a title element
+var title = new Title('Welcome to my Game'); // Create a title element
 var button = new Button('hello', btnPress);   // Create a button element
-var hidden = new Button('Boo!');
+var hidden = new Button('');
 hidden.hide();
 var scr = new Text(score); // Create a text element
 setInterval(btnPress, 1000);
